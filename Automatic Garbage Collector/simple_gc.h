9f8b9bf174d49f5ddb10470af2993c1cae0db7c2
NATHAN JOHN CORDEIRO <155100838+NathanCordeiro@users.noreply.github.com>
2024-08-14 00:16:11 +0530

    Update simple_gc.h
    new types creation and type conversions

--- Automatic Garbage Collector/simple_gc.h
+++ Automatic Garbage Collector/simple_gc.h
@@ -9,7 +9,12 @@
 
 typedef enum {
     VALUE_INT,
-    VALUE_PAIR
+    VALUE_FLOAT,
+    VALUE_DOUBLE,
+    VALUE_CHAR,
+    VALUE_PAIR,
+    VALUE_ENUM,
+    VALUE_UNION
 } ValueType;
 
 typedef struct GCValue {
@@ -17,24 +22,33 @@ typedef struct GCValue {
     unsigned char marked;
     struct GCValue* next;
     union {
-        // VALUE_INT
         int intValue;
-        // VALUE_PAIR
+        float floatValue;
+        double doubleValue;
+        char charValue;
         struct {
             struct GCValue* head;
             struct GCValue* tail;
         } pairValue;
+        int enumValue;
+        struct {
+            int tag;
+            union {
+                int intMember;
+                float floatMember;
+                double doubleMember;
+                char charMember;
+            } data;
+        } unionValue;
     };
 } GCValue;
 
 typedef struct {
     GCValue* stack[STACK_MAX];
     int stackSize;
-
     GCValue* firstValue;
     size_t numValues;
     size_t maxValues;
-
     uint8_t* heapStart;
     uint8_t* heapEnd;
     uint8_t* freePtr;
@@ -49,8 +63,19 @@ void markAllValues(GCHeap* heap);
 void sweepUnmarkedValues(GCHeap* heap);
 void collectGarbage(GCHeap* heap);
 
-// Helper functions for creating specific value types
+// Creation functions
 GCValue* createIntValue(GCHeap* heap, int value);
+GCValue* createFloatValue(GCHeap* heap, float value);
+GCValue* createDoubleValue(GCHeap* heap, double value);
+GCValue* createCharValue(GCHeap* heap, char value);
 GCValue* createPairValue(GCHeap* heap, GCValue* head, GCValue* tail);
+GCValue* createEnumValue(GCHeap* heap, int enumValue);
+GCValue* createUnionValue(GCHeap* heap, int tag, void* data);
+
+// Type conversion functions
+GCValue* convertToInt(GCHeap* heap, GCValue* value);
+GCValue* convertToFloat(GCHeap* heap, GCValue* value);
+GCValue* convertToDouble(GCHeap* heap, GCValue* value);
+GCValue* convertToChar(GCHeap* heap, GCValue* value);
 
 #endif // SIMPLE_GC_H
